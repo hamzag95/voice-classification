ac992527a82f96bbee6ecb3d5d9b52c72d6da73b
Hamza Ghani <hamza.ghani@utexas.edu>
2017-12-11 09:56:20 -0600

    code for demo

---
+++ scripts/demo/learn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sox -d $1.wav rate 32k silence 1 0.1 3% 1 3.0 3%
+mkdir -p ./train/$1
+mv $1.wav ./train/$1
+
+ffmpeg -i ./train/$1/$1.wav -f segment -segment_time 5 -c copy ./train/out%03d.wav
+
+for file in ./train/$1/*.wav
+do
+    outfile=${file%.*}
+	sox "$file" ${outfile}.r.wav remix 2
+	sox "$file" ${outfile}.l.wav remix 1
+    sox -m ${outfile}.l.wav ${outfile}.r.wav ${outfile}.mono.wav
+	sox ${outfile}.mono.wav -n spectrogram -r -o ${outfile}.mono.png
+	rm ${outfile}.l.wav ${outfile}.r.wav ${outfile}.mono.wav
+done
---
+++ scripts/demo/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sox -d $1.wav rate 32k silence 1 0.1 3% 1 3.0 3%
+mkdir -p ./test/$1
+mv $1.wav ./test/$1
+
+ffmpeg -i ./test/$1/$1.wav -f segment -segment_time 5 -c copy ./test/out%03d.wav
+
+for file in ./test/$1/*.wav
+do
+    outfile=${file%.*}
+	sox "$file" ${outfile}.r.wav remix 2
+	sox "$file" ${outfile}.l.wav remix 1
+    sox -m ${outfile}.l.wav ${outfile}.r.wav ${outfile}.mono.wav
+	sox ${outfile}.mono.wav -n spectrogram -r -o ${outfile}.mono.png
+	rm ${outfile}.l.wav ${outfile}.r.wav ${outfile}.mono.wav
+done
